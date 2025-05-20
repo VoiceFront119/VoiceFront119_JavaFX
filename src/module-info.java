34c85eca53272a788c2df0ac2c9958a7ad4385d0
UoUserusername <117015040+UoUserusername@users.noreply.github.com>
2025-05-20 19:34:01 +0900

    Delete src directory

--- src/module-info.java
+++
@@ -1,9 +0,0 @@
-module voicefront119 {
-	requires javafx.controls;
-	requires javafx.fxml;
-	requires javafx.graphics;
-	requires java.sql;
-	requires java.net.http;
-	
-	opens application to javafx.graphics, javafx.fxml;
-}
