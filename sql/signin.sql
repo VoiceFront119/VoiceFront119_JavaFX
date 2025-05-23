09c1e9c1d7aa2352700086c4f9ac60c5d34ce7a9
tmdgus11 <127588670+tmdgus11@users.noreply.github.com>
2025-05-23 19:02:33 +0900

    Create signin.sql

---
+++ sql/signin.sql
@@ -0,0 +1,11 @@
+CREATE TABLE IF NOT EXISTS users (
+    id INT NOT NULL AUTO_INCREMENT,
+    user_ID VARCHAR(20) NOT NULL,
+    user_name VARCHAR(15) NOT NULL,
+    password VARCHAR(40) NOT NULL,
+    user_phone VARCHAR(15) NOT NULL,
+    birth_date DATE NULL,
+    profile_image VARCHAR(225) NULL,
+    PRIMARY KEY (id),
+    UNIQUE INDEX username_UNIQUE (user_ID ASC) VISIBLE
+);
